2ff211ca284a6530f24fbe1e8fd6a50ec6ea6647
Deepak G <deepakavya17@gmail.com>
2026-02-06 22:38:50 +0530

    Time: 12 ms (59.55%), Space: 47.4 MB (52.8%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,53 @@
+class Solution{
+    public List<Integer> findSubstring(String s,String[] words){
+        List<Integer> result=new ArrayList<>();
+
+        if(words.length==0||s.length()==0){
+            return result;
+        }
+
+        int wordLen=words[0].length();
+        int totalLen=wordLen*words.length;
+        Map<String,Integer> freq=new HashMap<>();
+
+        for(String w:words){
+            freq.put(w,freq.getOrDefault(w,0)+1);
+        }
+
+        for(int i=0;i<wordLen;i++){
+            int left=i;
+            int count=0;
+            Map<String,Integer> seen=new HashMap<>();
+
+            for(int right=i;right+wordLen<=s.length();right+=wordLen){
+                String word=s.substring(right,right+wordLen);
+
+                if(freq.containsKey(word)){
+                    seen.put(word,seen.getOrDefault(word,0)+1);
+                    count++;
+
+                    while(seen.get(word)>freq.get(word)){
+                        String leftWord=s.substring(left,left+wordLen);
+                        seen.put(leftWord,seen.get(leftWord)-1);
+                        left+=wordLen;
+                        count--;
+                    }
+
+                    if(count==words.length){
+                        result.add(left);
+                    }
+
+                }else{
+                    seen.clear();
+                    count=0;
+                    left=right+wordLen;
+                }
+
+            }
+
+        }
+
+        return result;
+    }
+
+}
