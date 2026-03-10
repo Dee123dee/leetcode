6fa82cdb12f20cfebe0ba638209b24c113e61d21
Deepak G <deepakavya17@gmail.com>
2026-03-10 10:15:35 +0530

    Time: 0 ms (100%), Space: 43 MB (67.16%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,37 +1,46 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        
-        int n = nums.length;
-        
-        int[] prefix = new int[n+1];
-        
-        for(int i=1;i<=n;i++)
-        {
-            prefix[i] = prefix[i-1] + nums[i-1];
+
+        int left = 0;
+        int right = 0;
+
+        for (int n : nums) {
+            left = Math.max(left, n);
+            right += n;
         }
-        
-        int[][] dp = new int[n+1][k+1];
-        
-        for(int i=1;i<=n;i++)
-        {
-            dp[i][1] = prefix[i];
-        }
-        
-        for(int j=2;j<=k;j++)
-        {
-            for(int i=1;i<=n;i++)
-            {
-                dp[i][j] = Integer.MAX_VALUE;
-                
-                for(int p=0;p<i;p++)
-                {
-                    int largest = Math.max(dp[p][j-1], prefix[i]-prefix[p]);
-                    
-                    dp[i][j] = Math.min(dp[i][j], largest);
-                }
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (canSplit(nums, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        
-        return dp[n][k];
+
+        return left;
+    }
+
+    private boolean canSplit(int[] nums, int k, int maxSum) {
+
+        int count = 1;
+        int curr = 0;
+
+        for (int n : nums) {
+
+            curr += n;
+
+            if (curr > maxSum) {
+                count++;
+                curr = n;
+
+                if (count > k)
+                    return false;
+            }
+        }
+
+        return true;
     }
 }
