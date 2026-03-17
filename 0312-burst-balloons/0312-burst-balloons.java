e3518b46bec51324dbafcb1ad809124871a0c1e1
Deepak G <deepakavya17@gmail.com>
2026-03-17 23:00:33 +0530

    Time: 36 ms (99.25%), Space: 44.4 MB (78.04%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,37 +1,26 @@
 class Solution {
-    int[][] dp;
-int[] arr;
-public int maxCoins(int[] nums) {
-    int n = nums.length;
-    arr = new int[n + 2];
-    // Step 1: Create new array with boundaries
-    for(int i = 1; i <= n; i++){
-        arr[i] = nums[i - 1];
+    public int maxCoins(int[] nums) {
+        int[] arr = new int[nums.length + 2];
+        for(int i = 1; i < arr.length - 1; i++){
+            arr[i] = nums[i - 1];
+        }
+        arr[0] = 1;
+        arr[arr.length - 1] = 1;
+        int[][] dp = new int[arr.length][arr.length];
+        return burstBalloons(arr, arr.length - 1, dp);
     }
-    arr[0] = 1;
-    arr[n + 1] = 1;
-    
-    dp = new int[arr.length][arr.length];
-    // Solve for the entire array (from boundary 0 to boundary n+1)
-    return coins(0, arr.length - 1);
-}
-
-private int coins(int i, int j){
-    // Base Case: No balloons between i and j
-    if(i + 1 == j){
-        return 0;
+    public int burstBalloons(int[] arr, int n, int[][] dp){
+        for(int gap = 2; gap < arr.length; gap++){
+            for(int s = 0; s <= arr.length - 1 - gap; s++){
+                int e = s + gap;
+                int ans = Integer.MIN_VALUE;
+                for(int i = s + 1; i < e; i++){
+                    int myans = dp[s][i] + dp[i][e] + arr[s] * arr[i] * arr[e];
+                    ans = Math.max(myans, ans);
+                }
+                dp[s][e] = ans;
+            }
+        }
+        return dp[0][arr.length - 1];
     }
-    // Memoization check
-    if(dp[i][j] != 0){
-        return dp[i][j];
-    }
-    int maxCost = Integer.MIN_VALUE;
-    // Try every balloon k in (i, j) as the last burst
-    for(int k = i + 1; k < j; k++){
-        // Recursively solve left and right, then add the last burst cost.
-        maxCost = Math.max(coins(i, k) + coins(k, j) + arr[i] * arr[k] * arr[j], maxCost);
-    }
-    // Store the result and return
-    return dp[i][j] = maxCost;
-}
 }
