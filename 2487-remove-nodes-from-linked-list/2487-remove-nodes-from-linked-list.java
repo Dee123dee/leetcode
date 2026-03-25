2c355835bd868e16ff1bab53082a42933f6ddc90
Deepak G <deepakavya17@gmail.com>
2026-03-25 12:16:59 +0530

    Time: 7 ms (72.45%), Space: 129.8 MB (97.17%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+    
+    public ListNode removeNodes(ListNode head) {
+        if (head == null) return null;
+        head = reverse(head);
+        int max = head.val;
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null) {
+            if (curr.next.val < max) {
+                curr.next = curr.next.next; 
+            } else {
+                curr = curr.next;
+                max = curr.val;
+            }
+        }
+        return reverse(head);
+    }
+}
