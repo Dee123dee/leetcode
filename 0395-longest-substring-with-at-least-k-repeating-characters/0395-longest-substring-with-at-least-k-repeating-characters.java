0b7f869efd5afc0f742e87f37f22e6494766e8bc
Deepak G <deepakavya17@gmail.com>
2026-07-10 23:59:45 +0530

    Time: 571 ms (6.8%), Space: 47.7 MB (5.93%) - LeetHub

---
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int longestSubstring(String s, int k) {
+         if(s.isEmpty() ||s.length()<k)return 0;
+        Map<Character,Integer>map=new HashMap<>();
+        for(char c:s.toCharArray())map.put(c,map.getOrDefault(c,0)+1);
+        for(int i=0;i<s.length();i++){
+            if(map.get(s.charAt(i))<k){
+                int left=longestSubstring(s.substring(0,i),k);
+                int right=longestSubstring(s.substring(i+1),k);
+                return Math.max(left,right);
+            }
+        }
+        return s.length();}}
